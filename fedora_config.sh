4e00d4d7e6dc1030b32a9e68ce3e922ddba8b876
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-02-29 12:12:27 +0200

    Update fedora_config.sh

--- fedora_config.sh
+++ fedora_config.sh
@@ -3,7 +3,7 @@
 sudo dnf update
 
 # RPM fusion
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf update
 
