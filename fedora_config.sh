59d6a443c80c33561198400e1e22262ab5ae1df7
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-03-14 21:12:11 +0200

    Update fedora_config.sh

--- fedora_config.sh
+++ fedora_config.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-sudo dnf -y update
-
+## REPOSITORIES ##
 # RPM fusion
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 sudo dnf -y update
 
-# Apps
+
+
+## APPS ##
 sudo dnf install -y  terminator
 sudo dnf install -y git
 sudo dnf install -y gcc
@@ -18,6 +18,17 @@ sudo dnf install -y libreoffice
 sudo dnf install -y qbittorrent
 sudo dnf install -y notepadqq
 
+sudo dnf install -y logisim
+sudo dnf install -y htop
+sudo dnf install -y timeshift
+sudo dnf install -y stacer
+sudo dnf install -y steam
+sudo dnf install -y clamav
+sudo dnf install -y clamtk
+
+
+
+## SPECIAL INSTALLATION ##
 # VS code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -29,34 +40,28 @@ wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_curr
 sudo dnf install -y ~/Downloads/google-chrome*
 rm ~/Downloads/google-chrome*
 
-sudo dnf install -y logisim
-sudo dnf install -y htop
-sudo dnf install -y timeshift
-sudo dnf install -y stacer
-sudo dnf install -y steam
-sudo dnf install -y clamav
-sudo dnf install -y clamtk
 
-#Stop Recreation of default folders
+
+## CONFIGURATION ##
+#Stop Recreation of default folders and remove them
 sudo nano /etc/xdg/user-dirs.conf
-
-#Make the Directories
 rm -rf ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
-mkdir ~/Files
+
+#Customize bash prompt
+mv color.sh ~/.scripts 
+cat ./source >> ~/.bashrc
 
 #Make Folders
-mkdir ~/Documents/
+mkdir ~/Files
 mkdir ~/Files/backups/
 mkdir ~/Files/temp
 mkdir ~/Files/code 
 mkdir ~/Files/github
 mkdir ~/.scripts
 
-#Customize bash prompt
-mv color.sh ~/.scripts 
-cat ./source >> ~/.bashrc
 
-#Others
+
+## GUI Configuration ##
 clear
 echo "other changes in changes.txt"
 echo "Shortcuts = switch desktops - meta + f(num); carry to desktop - ctrl + f(num); terminator - ctrl + alt + T" >> changes.txt
