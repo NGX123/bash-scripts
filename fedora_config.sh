9fbabef7382745f6a842f45a49b409fac7e1f7eb
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-03-15 15:56:50 +0200

    Update fedora_config.sh

--- fedora_config.sh
+++ fedora_config.sh
@@ -60,7 +60,9 @@ rm -rf ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
 mv color.sh ~/.scripts 
 echo '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh' >> ~/.bashrc
 
-
+#Aliases file
+touch ~/.scripts/bash_aliases.sh
+echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> ~/.bashrc
 
 
 
