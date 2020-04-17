b574a5486b11053432917ecc56bec4f755e3c360
ngx123 <nikitja132@gmail.com>
2020-04-17 11:12:51 +0300

    b

--- scripts/bash_aliases.sh
+++ scripts/bash_aliases.sh
@@ -1,15 +1,16 @@
 bashs=~/.scripts/bash_scripts
+python_scripts=~/.scripts/python_scripts
 
 #Alias adder
 alias adda=$bashs/alias_adder.sh
 
 #Script Aliases
-
+alias weather='python $python_scripts/weatherbot.py'
 
 #Manual Aliases
 alias la='ls -a'
 alias lla='ls -al'
-
+alias jdump='journalctl | tail -n 100 >> journalctl_`date +%d-%m-%Y`_`date +%T`.dump'
 
 #Added Aliases
-
+alias rmi='rm -i'
---
+++ scripts/bash_scripts/desktop.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+echo "switch to desktop 1"
+echo "Broswer with firefly, gmail and gdrive"
+
+echo "switch to desktop 2"
+code ~/Files/github/tutorils-repo/programming/c/tasks
+code ~/Files/github/tutorils-repo/programming/c/let-us-c
+echo "browser with tasks"
+terminator
+
+echo "switch to desktop 3"
+echo "os 0 to 1"
+code ~/Files/github/zxos-repo
+terminator
+
