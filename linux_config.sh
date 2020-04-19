9443d3678ddd286a47d38ba0797046015b447723
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-04-19 20:04:47 +0300

    Update linux_config.sh

--- linux_config.sh
+++ linux_config.sh
@@ -74,7 +74,7 @@ mkdir ~/Files/code
 mkdir ~/Files/github
 
 ## Terminal Configurations ##
-cp ./scripts/* ~/.scripts
+cp -r ./scripts/* ~/.scripts
 echo '[ -f $HOME/.scripts/bash_scripts/color.sh ] && . $HOME/.scripts/bash_scripts/color.sh' >> ~/.bashrc
 echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> ~/.bashrc
 
