994107d1d93aaf6d845f45419c213094a7bc428c
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-06-06 09:27:36 +0300

    Update bash_aliases.sh

--- scripts/bash_aliases.sh
+++ scripts/bash_aliases.sh
@@ -1,16 +1,14 @@
 bashs=~/.scripts/bash_scripts
 python_scripts=~/.scripts/python_scripts
 
-#Alias adder
-alias adda=$bashs/alias_adder.sh
-
 #Script Aliases
-alias weather='python $python_scripts/weatherbot.py'
+alias adda=$bashs/alias_adder.sh
+alias backup='bash $bashs/backup.sh'
 
 #Manual Aliases
 alias la='ls -a'
 alias lla='ls -al'
-alias jdump='journalctl | tail -n 100 >> journalctl_`date +%d-%m-%Y`_`date +%T`.dump'
+#alias cross='$HOME/opt/cross-compiler/bin/i686-elf-gcc'
 
 #Added Aliases
 alias rmi='rm -i'
