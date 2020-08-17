486def8fe81d96bf06ce51f8a59977691fc03fdf
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-08-17 22:34:59 +0300

    Update fixes.sh

--- bash/fixes.sh
+++ bash/fixes.sh
@@ -3,3 +3,10 @@ export PATH="$PATH:/sbin:/usr/sbin:usr/local/sbin"
 
 # User not in sudoers file - enter the following line as root into terminal and reboot
 adduser <username> sudo
+
+# Fedora nvidia drivers
+sudo dnf update
+sudo dnf install nvidia-akmod
+reboot
+lsmod | grep nvidia           # check if nvidia modules are running
+# check if nvidia control panel is an application
