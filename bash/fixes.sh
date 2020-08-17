1b8cf5e0dd6e4a529ccbec7cc0f7e93334d2e1e9
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-08-17 22:28:40 +0300

    Create fixes.sh

---
+++ bash/fixes.sh
@@ -0,0 +1,5 @@
+# Binaries(programms) can't be found, add to .bashrc the following 
+export PATH="$PATH:/sbin:/usr/sbin:usr/local/sbin"
+
+# User not in sudoers file - enter the following line as root into terminal and reboot
+adduser <username> sudo
