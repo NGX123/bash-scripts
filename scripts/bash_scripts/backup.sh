f9ba159ee0935f4469f35183dd2619d761408e01
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-08-21 13:31:34 +0300

    Update backup.sh

--- scripts/bash_scripts/backup.sh
+++ scripts/bash_scripts/backup.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-cd ~/Files/github/c-repo
+cd ~/Files/github/tos
 git add .
 git commit -m "backup"
-git push c master
-
+git push tos master
 
 cd ~/Files/github/tutorials-repo
 git add .
 git commit -m "backup"
 git push t master
+
+cd ~/Files/github/nuxos-repo
+git add .
+git commit -m "backup"
+git push n master
