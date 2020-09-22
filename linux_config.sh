c118ca98e0c4b95a958163b0c523b8d2fcf1919b
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-09-22 19:27:47 +0300

    Update linux_config.sh

--- linux_config.sh
+++ linux_config.sh
@@ -161,7 +161,7 @@ if [ $de == gnome ]; then
     Search - Turn off search(application search stays)
     Privacy - Turn off everything except Screen Lock 
     Power - Automatic suspend, blank screen, dim when inactive
-    Devices - Keyboard Shortcuts - super + e = nautilus, ctrl + alt + t = terminator, ctrl + shift + esc = gnome-system monitor, super + f* = switch to workspace *, ctrl + f* = move window to workspace *
+    Devices - Keyboard Shortcuts - super + e = nautilus, ctrl + alt + t = terminator, ctrl + shift + esc = gnome-system monitor, super + f* = switch to workspace *, ctrl + f* = move window to workspace *, ctrl + space = change input language
     Details - Default Apps - Set" >> changes.txt
     cat changes.txt
 fi
