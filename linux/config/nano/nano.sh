50cc69082ac77f06ccd3714bd314afc31faf87c1
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-10-16 08:41:38 +0300

    Update nano.sh

--- linux/config/nano/nano.sh
+++ linux/config/nano/nano.sh
@@ -1,8 +1,5 @@
 # Make directory for user-local configuration
 mkdir $HOME/.config/nano
 
-# Copy files 
+# Copy files to the config folder
 cp ./nano/* $HOME/.config/nano
-
-# Copy the nano configuration to user dir
-cp ./nanorc $HOME/.nanorc
