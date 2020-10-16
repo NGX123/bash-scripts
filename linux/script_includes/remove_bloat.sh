8f8bddef090361d9a6fd7a5300c351fc14522f51
NGX <nikitja132@gmail.com>
2020-10-16 23:22:42 +0300

    small changes

--- linux/script_includes/remove_bloat.sh
+++ linux/script_includes/remove_bloat.sh
@@ -1,8 +1,8 @@
 # File that includes bloat removal code for the main file to run it
 
-if [ $removeBloat_var == y ]; then 
+if [ $removeBloat_var == y ]; then
     if [ $de_var == kde ]; then
-        sudo $pm_var $rmc -y calligra-sheets calligra-stage calligra-words dragon juk k3b kamoso kmail kaddressbook kamera kget ktorrent kmahjongg kmines kolourpaint kpat kwalletmanager #kde-connect kdeconnect konqueror krdc
+        sudo $pm_var $rmc -y calligra-sheets calligra-stage calligra-words dragon juk k3b kamoso kmail kaddressbook kamera kget ktorrent kmahjongg kmines kolourpaint kpat # kwalletmanager kde-connect kdeconnect konqueror krdc
     fi
 
     if [ $de_var == gnome ]; then
@@ -12,13 +12,13 @@ fi
 
 ## Remove ssh-server
 if [ $removeSshd_var == y ]; then
-    if [ $pm_var == dnf ]; then 
+    if [ $pm_var == dnf ]; then
         sudo chkconfig sshd off
         sudo service sshd stop
         sudo dnf erase openssh-server
     fi
-    
-    if [ $pm_var == apt ]; then 
+
+    if [ $pm_var == apt ]; then
         sudo apt-get --purge remove openssh-server
     fi
 fi
@@ -29,4 +29,3 @@ if [ $removeFldrs_var == y ]; then
     cat ./script_includes/dirs > ~/.config/user-dirs.dirs
     rm -rf ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
 fi
-
