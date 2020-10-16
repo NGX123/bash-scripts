66f5dc059b0648a4ee87723c0bf2f21afb10f9d3
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-10-16 18:42:01 +0300

    Update vscode.sh

--- linux/config/vscode/vscode.sh
+++ linux/config/vscode/vscode.sh
@@ -6,6 +6,14 @@ code --install-extension ms-python.python
 
 read -p "Platform: " platform_var
 
+if [ $platform_var == macos ]; then
+    cat ./settings.json >> $HOME/Library/Application Support/Code/User/settings.json
+fi
+
+if [ $platform_var == linux ]; then
+    cat ./settings.json >> $HOME/.config/Code/User/settings.json
+fi
+
 # Configuration articles
     # https://dev.to/iamismile/my-vs-code-setup-4fcd
     # https://code.visualstudio.com/docs/editor/command-line
