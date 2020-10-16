1426f57d362c43c044f288d9f85fccde7666c6cc
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-10-16 18:36:15 +0300

    Add files via upload

---
+++ linux/config/vscode/vscode.sh
@@ -0,0 +1,12 @@
+# Install vscode extensions
+# Format author.extension(this info is displayed next to extension name on it's store page)
+code --install-extension aaronthomas.vscode-snazzy-operator
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-python.python
+
+read -p "Platform: " platform_var
+
+# Configuration articles
+    # https://dev.to/iamismile/my-vs-code-setup-4fcd
+    # https://code.visualstudio.com/docs/editor/command-line
+    # https://calebporzio.com/my-vs-code-setup-2
