a6c646385450987cde35fd5c5e5f13b8b418ae80
NGX123 <nikitja132@gmail.com>
2020-10-18 14:15:41 +0300

    removed aliases with references to old files

--- config/bash/bash_aliases.sh
+++ config/bash/bash_aliases.sh
@@ -1,14 +1,7 @@
 # Files
 [ -f $HOME/.scripts/bash_functions.sh ] && . $HOME/.scripts/bash_functions.sh || echo 'Function file does not exist' 
 
-#Script Aliases
-alias adda=$bashs/alias_adder.sh
-alias backup='bash $bashs/backup.sh'
-
-#Manual Aliases
+# Aliases
 alias la='ls -a'
 alias lla='ls -al'
 #alias cross='$HOME/opt/cross-compiler/bin/i686-elf-gcc'
-
-#Added Aliases
-alias rmi='rm -i'
