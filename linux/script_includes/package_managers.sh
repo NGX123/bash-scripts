cd561daff6d7a1d3493b5faadc2f9cd649f32750
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-11-05 07:45:29 +0200

    Update package_managers.sh

--- linux/script_includes/package_managers.sh
+++ linux/script_includes/package_managers.sh
@@ -12,7 +12,7 @@ if [ $pm_var == apt ]
         sudo apt -y update && sudo apt -y upgrade
 
         # Install any dependencies
-        sudo apt -y install curl
+        sudo apt -y install curl wget
 
         # VS code
         curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -21,6 +21,11 @@ if [ $pm_var == apt ]
         sudo apt-get install apt-transport-https
         sudo apt-get update
         sudo apt-get install code
+
+        # Google Chrome
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+        rm ./google-chrome-stable_current_amd64.deb
 
         # Apps(might not install becuase apt requires all of them to be in repos, otherwise it fails)
         sudo apt-get install -y $apps_list
@@ -42,6 +47,9 @@ if [ $pm_var == dnf ]
 
         # Update
         sudo dnf -y update
+        
+        # Dependencies
+        sudo dnf -y install curl wget
 
         # RPM Fusion
         sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -52,6 +60,11 @@ if [ $pm_var == dnf ]
         sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
         sudo dnf -y update
         sudo dnf install -y code
+
+        # Google Chrome
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+        sudo dnf install -y ./google-chrome-stable_current_*.rpm
+        rm ./google-chrome-stable_current_*.rpm
 
         # Apps
         sudo dnf install -y $apps_list
