3557b8d15b972b88211efa6d34372c3f92c4bef4
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-12-06 11:36:39 +0200

    Update dev-setup.sh

--- config/misc/dev-setup.sh
+++ config/misc/dev-setup.sh
@@ -1,3 +1,5 @@
+install_list_var="nasm binutils diffutils valgrind clang gcc"
+
 # Installs programms for the osdev
 read -p "Package Manager(dnf, apt, macos): " pm_var
 
@@ -5,103 +7,19 @@ read -p "Package Manager(dnf, apt, macos): " pm_var
 # DNF Installations
 if [ $pm_var == dnf ]
   then
-    # Kernel headers(for linux device drivers)
-    sudo dnf install kernel-headers kernel-devel
-
-    # Packages
-    sudo dnf -y install binutils diffutils @development-tools nasm valgrind
-
-    # Install the dependencies for cross-compiler
-    sudo dnf -y install gcc gcc-c++ make bison flex gmp-devel libmpc-devel mpfr-devel texinfo automake autoconf xorriso
+   # Install developer tools and headers
+    sudo dnf -y install $install_list_var @development-tools kernel-headers kernel-devel
 fi
 
 # APT Installations
 if [ $pm_var == apt ]
   then
-    # Kernel headers
-    sudo apt -y install linux-headers-$(uname -r) # If does not work check "ls -l /usr/src/linux-headers-$(uname -r)"(if does not exist then there are no headers), insetad try to find the latest version if not installed
-
-    # Packages
-    sudo apt -y install build-essential nasm binutils diffutils valgrind
-
-    # Dependencies
-    sudo apt -y install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo
+    # Install developer tools and headers
+    sudo apt -y install $install_list_var build-essential linux-headers-$(uname -r)
 fi
 
 if [ $pm_var == macos ]
   then
     # Packages
     brew install gdb nasm binutils diffutils
-
-    # Dependencies
-    brew install gmp mpfr libmpc libiconv
-    brew install libiconv
-    echo "There maybe problems during the compilation proccess due to wrong attributes, then remove -with-sysroot and add --enable-interwork to both gcc and binutils"
 fi
-
-
-
-
-## i386 CROSS COMPILER ##
-# Create directories to store source and build directory
-mkdir -p ~/src/cross-compiler/binutils2.30/build
-mkdir -p ~/src/cross-compiler/gcc9.3.0/build
-
-# Download and unpack source code
-cd ~/src/cross-compiler/binutils2.30/
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.gz
-tar -xzf binutils-2.30.tar.gz
-rm binutils-2.30.tar.gz
-
-cd ~/src/cross-compiler/gcc9.3.0
-wget https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.gz
-tar -xzf gcc-9.3.0.tar.gz
-rm gcc-9.3.0.tar.gz
-
-# Get ready for the build
-mkdir -p ~/opt/
-export PREFIX="$HOME/opt/"
-export TARGET=i686-elf
-export PATH="$PREFIX/bin:$PATH"
-
-# Compilation instructions
-if [ $pm_var != macos ]
-  then
-    # Building binutils
-    cd $HOME/src/cross-compiler/binutils2.30/build
-    ../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-    make
-    make install
-
-    # Building GCC
-    cd $HOME/src/cross-compiler/gcc9.3.0/build
-    which -- $TARGET-as || echo $TARGET-as is not in the PATH
-    ../gcc-9.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-language=c,c++ --without-headers
-    make all-gcc
-    make all-target-libgcc
-    make install-gcc
-    make install-target-libgcc
-  else
-    # Building binutils
-    cd $HOME/src/cross-compiler/binutils2.30/build
-    ../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror --enable-multilib --with-libiconv-prefix=/usr/local/opt/libiconv/
-    make
-    make install
-
-    # Building GCC
-    cd $HOME/src/cross-compiler/gcc9.3.0/build
-    which -- $TARGET-as || echo $TARGET-as is not in the PATH
-    ../gcc-9.3.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-language=c,c++ --without-headers --enable-multilib --with-libiconv-prefix=/usr/local/opt/libiconv/
-    make all-gcc
-    make all-target-libgcc
-    make install-gcc
-    make install-target-libgcc
-fi
-
-
-
-
-# CHECK OF INSTALLTION
-qemu-system-i386 --version
-$TARGET-gcc --version
-grub2-mkrescue --version
