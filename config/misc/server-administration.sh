1b92bda40a26a0621ad07ad545e7dc5b5b35b332
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-12-29 10:11:51 +0200

    Update server-administration.sh

--- config/misc/server-administration.sh
+++ config/misc/server-administration.sh
@@ -3,6 +3,7 @@ sudo system ctl mask sleep.target suspend.target # Hibernation(hibernate.target 
 reboot
 systemctl status sleep.target suspend.target # check for result
 
+
 ## Setup SSH ##
 # Debian-based
 sudo apt install -y ssh-server
@@ -10,6 +11,19 @@ sudo systemctl status ssh
 # sudo ufw allow ssh
 sudo systemctl enable ssh
 
+
 ## Monitor temperature ##
 sudo apt install hardinfo
 hardinfo -rma devices.so
+
+
+## Remove smb
+sudo systemctl stop smb && sudo <package manager> remove samba samba-libs smaba-common-tools
+
+
+## Remove ssh
+# RHEL/Fedora
+chkconfig sshd off && service sshd stop && sudo dnf erase openssh-server
+# Debian
+sudo apt-get --purge remove openssh-server
+# Now disable access to tcp port 22(ssh) in firewall 
