5dc709a228bea5c5fcf70984cf946511fb4b6644
ngx123 <49251986+NGX123@users.noreply.github.com>
2020-12-30 08:39:24 +0200

    Update virtualbox_setup.sh

--- macos/virtualbox_setup.sh
+++ macos/virtualbox_setup.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 
-read -p "Do you want to setup NAT adapter(y/n): " network_variable
-
 # Messages
 touch setup.txt
 > setup.txt
 
-if [ $network_variable == y ]
-  then
-    echo "
-    + Virtualbox -> VM -> Settings -> Network -> Adapter 1 -> Advanced -> Port Forwarding -> Add(+) -> name(ssh), protocol(TCP), host ip(127.0.0.1), host port(any free e.g. 5679), guest ip(10.0.2.15), guest port(22) -> Press OK
-    + To SSH from host use - ssh -p 5679 127.0.0.1"
-fi
+
 echo "
- + Vbox -> VM -> Settings -> Shared Folders -> Add new folder -> Select path to folder, Auto-mount(ON), Mount point(/media/shared_dir)
- + VM -> Devices -> Insert guest iso
- + VM(Terminal) -> su" >> setup.txt
++ Virtualbox -> VM -> Settings -> Network -> Adapter 1 -> Advanced -> Port Forwarding -> Add(+) -> name(ssh), protocol(TCP), host ip(127.0.0.1), host port(any free e.g. 5679), guest ip(10.0.2.15), guest port(22) -> Press OK
++ To SSH from host use - ssh -p 5679 127.0.0.1
++ Vbox -> VM -> Settings -> Shared Folders -> Add new folder -> Select path to folder, Auto-mount(ON), Mount point(/media/shared_dir)
++ VM -> Devices -> Insert guest iso
++ VM(Terminal) -> su
++ VM(Terminal) -> cd ~/media/shared_dir && make toolchain" >> setup.txt
 cat setup.txt
 
 read -p "Is eveything listed done(y/n): " setupdone_variable
