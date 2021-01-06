359f07b5863c376145a637ad9356133b67b75883
NGX123 <nikitja132@gmail.com>
2021-01-06 21:26:15 +0200

    fixed non existing directory

--- scripts/include/global_linux_config.sh
+++ scripts/include/global_linux_config.sh
@@ -1,4 +1,5 @@
 # Terminal Configurations
+mkdir -p $HOME/.scripts
 cp -r $configDir_variable/apps/bash/* $HOME/.scripts
 [ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh') ] && echo '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh' >> $HOME/.bashrc
 [ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh') ] && echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> $HOME/.bashrc
