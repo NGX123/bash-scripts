4540d9acefea6134e7e23d1ac9cbef833eb2a8d6
NGX123 <nikitja132@gmail.com>
2021-01-06 18:56:35 +0200

    moved to another directory

---
+++ scripts/include/macos_config.sh
@@ -0,0 +1,15 @@
+# Folder creation
+mkdir -p $HOME/vm $HOME/opt $HOME/src $HOME/.config $HOME/github/tos-repo $HOME/github/programming-repo
+
+# Git configuration
+git config --global user.name "NGX123"
+git config --global user.email "NGX123"
+
+# User message
+# echo "
+# ---------------------------------------------------------------------------------------------------------
+# 1. Check if VScode, Transmission, iTerm2, Vbox, Geany, MPV, Qemu and Google Chrome were installed
+# 2. Check if vm, opt, src, .config, github folders were created
+# 3. Try pushing commits to private repos to check if macos uses the nickname(NGX123) and not the real name
+# ---------------------------------------------------------------------------------------------------------
+# "
