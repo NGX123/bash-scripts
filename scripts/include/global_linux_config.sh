1d8feaf0cd7dab2ff78cea74fd4fa5d264b0eb75
NGX123 <nikitja132@gmail.com>
2021-01-06 20:56:57 +0200

    file including things that should be done for all linux distros

---
+++ scripts/include/global_linux_config.sh
@@ -0,0 +1,9 @@
+# Terminal Configurations
+cp -r $configDir_variable/apps/bash/* $HOME/.scripts
+[ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh') ] && echo '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh' >> $HOME/.bashrc
+[ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh') ] && echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> $HOME/.bashrc
+
+# Remove default XDG folders
+mkdir -p $HOME/.stdfldrs
+cat $textfilesDir_variable/dirs > $HOME/.config/user-dirs.dirs
+rm -rf $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos
