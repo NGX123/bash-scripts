be7ab0c577bd08c1271ea732b9afb91d25a42c9f
NGX123 <nikitja132@gmail.com>
2021-01-06 21:02:56 +0200

    moved folder existance checks; added fail_function to exit and tell the problem; added platform specific configuration; moved the list of folders for creation into a variable; added checks if something is already in bashrc before writing; changed the message at the end to also include macos

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,72 +1,68 @@
 #! /bin/bash
 
-# Variables
+## Functions ##
+fail_function () {             # Called when program fails
+    clear
+    echo "--- Fail - $1 ---"
+    exit
+}
+
+
+
+
+## Variables ##
 apps_list="terminator mpv transmission firefox \
 nasm nano binutils diffutils valgrind clang"
+dirs_list="$HOME/src $HOME/opt/bin $HOME/github $HOME/vm/iso $HOME/vm/vms $HOME/.scripts"
+
 includesDir_variable=./include
 textfilesDir_variable=$includesDir_variable/textfiles
 configDir_variable=../config
 
-removeFldrs_var=n
-removeSshd_var=n
 
+
+
+## Script setup ##
 # Folder existance checks
-[ ! -d $includesDir_variable ] && echo "$includesDir_variable does not exist" && exit
-[ ! -d $textfilesDir_variable ] && echo "$textfilesDir_variable does not exist" && exit
-[ ! -d $configDir_variable ] && echo "$configDir_variable does not exist" && exit
+[ ! -d $includesDir_variable ] && fail_function "$includesDir_variable does not exist"
+[ ! -d $textfilesDir_variable ] && fail_function "$textfilesDir_variable does not exist"
+[ ! -d $configDir_variable ] && fail_function "$configDir_variable does not exist"
 
-
-
-
-## SCRIPT SETUP ##
-## User input
+# User input
 read -p "OS: " os_var
 read -p "Package Manager: " pm_var
 read -p "Destktop Environment: " de_var
-read -p "Configure install(y/n): " configureInstall_var
-
-# Extra script configuration
-if [ $configureInstall_var == y ]
-    then
-        read -p "Remove Folders(y/n): " removeFldrs_var
-        read -p "Remove ssh(y/n): " removeSshd_var
-fi
-
-# Package manager code
-[ -f $includesDir_variable/package_managers.sh ] && . $includesDir_variable/package_managers.sh
-
-[ -f $includesDir_variable/"$os_var"_config.sh ]
 
 
-## MISC ##
-# Make Folders
-mkdir -p $HOME/src $HOME/opt/bin $HOME/github $HOME/vm/iso $HOME/vm/vms $HOME/.scripts
 
-# Add the folder with compiled software to path
-echo 'PATH=$PATH:$HOME/opt/bin' >> $HOME/.bashrc
 
-# Terminal Configurations
-cp -r $configDir_variable/apps/bash/* $HOME/.scripts
-echo '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh' >> $HOME/.bashrc
-echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> $HOME/.bashrc
+## System-specific Configuration
+# Include platform specific scripts
+[ -f $includesDir_variable/"$os_var"_config.sh ] && . $includesDir_variable/"$os_var"_config.sh || fail_function "$includesDir_variable/"$os_var"_config.sh does not exist"                                                 # Distro/platform specific configs
+[ $(echo $os_var | grep linux) ] && [ -f $includesDir_variable/global_linux_config.sh ] && . $includesDir_variable/global_linux_config.sh || fail_function "$includesDir_variable/global_linux_config.sh does not exist"    # Configs that apply to all linuxes
 
-# Remove default XDG folders
-if [ $removeFldrs_var == y ]
-    then
-        mkdir -p $HOME/.stdfldrs
-        cat $textfilesDir_variable/dirs > $HOME/.config/user-dirs.dirs
-        rm -rf $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos
-fi
+# Include package manager script
+[ -f $includesDir_variable/package_managers.sh ] && . $includesDir_variable/package_managers.sh || fail_function "$includesDir_variable/package_managers.sh does not exist"
 
-# Clone git repos
+
+
+
+## Cross-platform ##
+# Make folders
+mkdir -p $dirs_list
+
+# Add local binary directory to PATH
+[ ! $(cat $HOME/.bashrc | grep 'PATH=$PATH:$HOME/opt/bin') ] && echo 'PATH=$PATH:$HOME/opt/bin' >> $HOME/.bashrc
+
+# Setup git repos
 mkdir -p $HOME/github/programming-repo
-cd $HOME/github/programming-repo
+cd $HOME/github/programming-repo || fail_function "Can't setup programming-repo"
 git init
 git remote add p https://github.com/NGX123/programming
 git pull p master
 
 mkdir -p $HOME/github/tos-repo
-cd $HOME/github/tos-repo
+cd $HOME/github/tos-repo || fail_function "Can't setup tos-repo"
 git init
 git remote add t https://github.com/NGX123/tos
 git pull t master
@@ -74,7 +70,7 @@ git pull t master
 
 
 
-## MESSAGES ##
+## Script End Messages ##
 # GUI Configurations
 if [ $de_var != 0 ]
     then
@@ -86,18 +82,20 @@ fi
 # Print the final message
 echo "
 --- Check if all points were done ---
-1. $apps_list were installed(they might not be installed if you use apt and one of the packages does not exist in repos, becuase for apt to work all packages supplied at once need to be in the repos)
-2. ~/src, ~/opt, ~/github, ~/.scripts folders were made
+1. All of the apps were installed by the package manager:
+    - On Linux - $apps_list(some or all might not be installed if apt is used becuase it fails if any package is missing)
+    - On MacOS - binutils diffutils nasm gdb nano cask mpv qemu geany code transmission iterm2 vbox firefox chrome
+2. Following folders were made - $dirs_list
 3. Local binary folder was added to PATH
-4. Custom scripts and system config files were added to ~/.scripts
-5. Bash aliases and function file inclusions were added to .bashrc
-6. Bash color scheme was changed by inclusion of config file in .bashrc
-7. Github repos were cloned to ~/github/programming-repo & ~/github/tos-repo
-
-8. (Optional) Default folders were deleted
-9. (Optional) SSH server was removed
+4. Github repos were cloned to ~/github/programming-repo & ~/github/tos-repo
+5. On Linux
+    - Default folders were removed
+    - Custom scripts and system config files were added to ~/.scripts
+    - Bash aliases and function file inclusions were added to .bashrc
+    - Bash color scheme was changed by inclusion of config file in .bashrc
+6. On MacOS
+    - Username and email were changed for git
 
 --- To do manually ---
 1. Configure the GUI
-2. Install all the VMs through QEMU to the ~/github/vm/vm
-3. (Optional) Download the books to ~/Documents/Books"
+2. Install all the VMs through QEMU to the ~/vm/vms
