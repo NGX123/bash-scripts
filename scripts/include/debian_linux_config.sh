d378205d5d4b1e9c12059c11a2eaa98ec6e03d64
NGX123 <nikitja132@gmail.com>
2021-01-07 10:16:36 +0200

    fixed script not going on after switching to root

--- scripts/include/debian_linux_config.sh
+++ scripts/include/debian_linux_config.sh
@@ -8,17 +8,26 @@ read -p "What is the username: " username_variable
 [ ! $(cat $HOME/.bashrc | grep 'PATH=$PATH:/sbin:/usr/sbin') ] && echo 'PATH=$PATH:/sbin:/usr/sbin' >> $HOME/.bashrc
 source $HOME/.bashrc
 
+
+
+
 # Make normal user sudo
-su
-apt update && apt upgrade
-apt install sudo
-/sbin/adduser username_variable sudo
+su -c "\
+apt update && apt upgrade \
+apt install sudo \
+/sbin/adduser $username_variable sudo"
+
+
+
 
 # Add non-free repositories
-apt-add-repository contrib
-apt-add-repository non-free
-apt update && sudo apt upgrade
-exit
+su -c "\
+apt-add-repository contrib \
+apt-add-repository non-free \
+apt -y update && sudo apt -y upgrade"
+
+
+
 
 clear
 read -p "--- If this is the first time the script is run reboot and rerun it, reboot(y/n): " reboot_var
