9197e811a98c950cd9ab04c8d8bd11c50bbaf0e3
ngx123 <49251986+NGX123@users.noreply.github.com>
2021-02-07 16:34:32 +0200

    Update bash_functions.sh

--- config/apps/bash/bash_functions.sh
+++ config/apps/bash/bash_functions.sh
@@ -1,4 +1,33 @@
-backup () {
+backup_data () {
+	current_date=$(date '+%d-%m-%Y')
+
+	# Check presence of google drive folders
+	if ls *gdrive* >> /dev/null
+		then
+			echo "GDrive Folders Found"
+		else
+			echo "GDrive Folders Not Found"
+			exit
+	fi
+
+	# Tar(but not compress) google drive folders
+	tar --verbose --create --file=gdrive_"$current_date".tar *gdrive*
+	#zip -0 -r google-drive_"$current_date".zip *gdrive* # zip the direcotries recursively without compression
+
+	# Clone the repositories
+	git clone https://github.com/NGX123/tos tos-repo
+	git clone https://github.com/NGX123/unix unix-repo
+	git clone https://github.com/NGX123/programming programming-repo
+
+	# Tar(but not compress) the repositories
+	tar --verbose --create --file=github_"$current_date".tar *repo
+	#zip -0 -r github_"$current_date".zip *repo
+
+	# Remove the leftover
+	rm -rf ./*gdrive* ./*repo
+}
+	
+push_github () {
 	cd ~/github/tos-repo
 	git add .
 	git commit -m "backup"
