97e24d8307be1436e4aaa34b8ecafd7d59967cb5
ngx123 <49251986+NGX123@users.noreply.github.com>
2021-04-12 10:15:38 +0300

    Update global_linux_config.sh

--- scripts/include/global_linux_config.sh
+++ scripts/include/global_linux_config.sh
@@ -3,7 +3,6 @@ mkdir -p $HOME/.scripts
 cp -r $configDir_variable/apps/bash/* $HOME/.scripts
 [ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh') ] && echo '[ -f $HOME/.scripts/color.sh ] && . $HOME/.scripts/color.sh' >> $HOME/.bashrc
 [ ! $(cat $HOME/.bashrc | grep '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh') ] && echo '[ -f $HOME/.scripts/bash_aliases.sh ] && . $HOME/.scripts/bash_aliases.sh' >> $HOME/.bashrc
-# PS1="\[\e[34m\]\u\[\e[m\] \[\e[33m\]\w\[\e[m\]\[\e[32m\] \[\e[m\]\[\e[32m\]\\$\[\e[m\] "
 
 # Remove default XDG folders
 mkdir -p $HOME/.stdfldrs
