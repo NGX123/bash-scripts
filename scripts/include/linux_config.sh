f7f9e789b5d07319a2e529967c1b806659269ef9
ngx123 <49251986+NGX123@users.noreply.github.com>
2021-05-16 12:58:30 +0300

    Update linux_config.sh

--- scripts/include/linux_config.sh
+++ scripts/include/linux_config.sh
@@ -1,4 +1,4 @@
 # Remove default XDG folders
-mkdir -p $HOME/.stdfldrs
-cat $textfilesDir_variable/dirs > $HOME/.config/user-dirs.dirs
-rm -rf $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos
+mkdir -p "$HOME"/.stdfldrs
+cat "$textfiles_dir"/dirs > "$HOME"/.config/user-dirs.dirs
+rm -rf "$HOME"/Music "$HOME"/Pictures "$HOME"/Public "$HOME"/Templates "$HOME"/Videos
